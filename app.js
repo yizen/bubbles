894c3b124bb3cdfd3f2fd26e51abdda2fc2adec6
Olivier Grenet <ogrenet@gmail.com>
2013-09-06 16:56:22 +0200

    Changed posts directory

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.configure(function(){
     //Poet blogging engine
 	var poet = Poet(app, {
 	  postsPerPage: 3,
-	  posts: './posts',
+	  posts: 'posts',
 	  metaFormat: 'json',
 	  routes: {
 	    '/blog/post/:post': 		'blog/post',
