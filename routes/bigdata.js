60f55ea3d6de470b963bf8175a9e108c4094f202
Olivier Grenet <ogrenet@gmail.com>
2013-09-06 10:53:50 +0200

    Enhanced bigdata

--- routes/bigdata.js
+++ routes/bigdata.js
@@ -1,6 +1,44 @@
+async 			= require('async');
+
 module.exports = function(app){
 	app.get('/bigdata/', function(req, res){
-		res.render('bigdata');
+			
+		async.parallel({
+			websiteCount: function(callback) {
+				db.Website.count().success(function(c) {
+					callback(null, c);
+				});
+			},
+			wineCount: function(callback) {
+				db.Wine.count().success(function(c) {
+					callback(null, c);
+				});
+			},
+			producerCount: function(callback) {
+				db.Producer.count().success(function(c) {
+					callback(null, c);
+				});
+			},
+			wineRefCount: function(callback) {
+				db.Winereference.count().success(function(c) {
+					callback(null, c);
+				});
+			},
+			meanPrice: function(callback) {
+				db.sequelize.query('select avg(price) as price from Wines').success(function(c){
+					var price = Math.floor(c[0].price);
+					callback(null, price);
+				});
+			}			
+		},
+		
+		function(err, results){
+			res.render('bigdata', {bigdata: results} );
+
+		});
+		
+		
+	
 	});
 	
 	app.get('/bigdata/mostFrequentProducers/', function(req, res){
